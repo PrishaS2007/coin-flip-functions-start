315b4264e4dc23b9ab7ca9d4aa95740a4dae55b2
Prisha Shreevastav <112982045+PrishaS2007@users.noreply.github.com>
2024-02-12 10:46:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,56 +8,22 @@ document.getElementById("btn").addEventListener("click", btnHandler);
 
 function btnHandler() {
   // First Coin Flip
-  let coin1;
-  if (Math.random() < 0.5) {
-    coin1 = "heads";
-  } else {
-    coin1 = "tails";
-  }
+  let coin1 = flipCoin();
 
   // Second Coin Flip
-  let coin2;
-  if (Math.random() < 0.5) {
-    coin2 = "heads";
-  } else {
-    coin2 = "tails";
-  }
+  let coin2 = flipCoin();
 
   // Third Coin Flip
-  let coin3;
-  if (Math.random() < 0.5) {
-    coin3 = "heads";
-  } else {
-    coin3 = "tails";
-  }
+  let coin3 = flipCoin();
 
   // Output Results
   outputEl.innerHTML = `<img src="img/${coin1}.png"> <img src="img/${coin2}.png"> <img src="img/${coin3}.png">`;
 }
 
 function flipCoin() {
-  let coin1;
   if (Math.random() < 0.5) {
-    coin1 = "heads";
+    return "heads";
   } else {
-    coin1 = "tails";
+   return "tails";
   }
-
-  // Second Coin Flip
-  let coin2;
-  if (Math.random() < 0.5) {
-    coin2 = "heads";
-  } else {
-    coin2 = "tails";
-  }
-
-  // Third Coin Flip
-  let coin3;
-  if (Math.random() < 0.5) {
-    coin3 = "heads";
-  } else {
-    coin3 = "tails";
-  }
- 
-  return 
 }
